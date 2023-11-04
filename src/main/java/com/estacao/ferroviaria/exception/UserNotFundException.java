d69d816d097fce9a430d8b7dade8c28ed95ef7c3
paulo-maculuve <paulomaculuve758@gmail.com>
2023-11-04 08:40:47 +0200

    adicionar modes user e roles

---
+++ src/main/java/com/estacao/ferroviaria/exception/UserNotFundException.java
@@ -0,0 +1,9 @@
+package com.estacao.ferroviaria.exception;
+
+public class UserNotFundException extends Throwable {
+
+	public UserNotFundException(String s) {
+		super(s);
+	}
+
+}
---
+++ src/main/java/com/estacao/ferroviaria/model/Users.java
@@ -0,0 +1,87 @@
+package com.estacao.ferroviaria.model;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	 @Column(nullable = false, length = 100)		
+	   private String nome;
+	    @Column(nullable = false, unique = true, length = 100)
+	    private String email;
+	    @Column(length = 15)
+	    private String telefone;
+	    @Column(nullable = false, length = 200)
+	    private String password;
+	     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	    @JoinTable(name = "users_roles",
+	        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+	            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+	    )
+	     
+	     public Set<Roles> roles;
+
+
+	     public Long getId() {
+	         return id;
+	     }
+
+	     public void setId(Long id) {
+	         this.id = id;
+	     }
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getTelefone() {
+			return telefone;
+		}
+
+		public void setTelefone(String telefone) {
+			this.telefone = telefone;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public Set<Roles> getRoles() {
+			return roles;
+		}
+
+		public void setRoles(Set<Roles> roles) {
+			this.roles = roles;
+		}
+
+
+}
